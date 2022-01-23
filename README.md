3fe7437e1d9fed0a84624128477a11add56dc932
Fatima Akhter <fa1249@my.bristol.ac.uk>
2022-01-23 21:16:01 +0000

    adding files from sagemaker

--- README.md
+++ README.md
@@ -1,41 +1,40 @@
 # Image Classification using AWS SageMaker
 
-Use AWS Sagemaker to train a pretrained model that can perform image classification by using the Sagemaker profiling, debugger, hyperparameter tuning and other good ML engineering practices. This can be done on either the provided dog breed classication data set or one of your choice.
+The work carried out is part of the AWS Machine Learning Engineer Nanodegree Program. It uses AWS Sagemaker to train a pretrained model that can perform image classification by using the Sagemaker profiling, debugger, hyperparameter tuning and other good ML engineering practices. It has been done with he provided dog breed classication data set. 
 
-## Project Set Up and Installation
-Enter AWS through the gateway in the course and open SageMaker Studio. 
-Download the starter files.
-Download/Make the dataset available. 
+The project has been carried out with the following: 
+1. Project Set Up and Installation: Download the data and upload to S3 to be accessed from Sagemaker. 
+2. Choose pre-train CNN and train model with hyperparameter tuning.
+3. Using the best parameters ,re-train the model with AWS Debugger and Profiler. 
+4. Deploy the model.
 
-## Dataset
-The provided dataset is the dogbreed classification dataset which can be found in the classroom.
-The project is designed to be dataset independent so if there is a dataset that is more interesting or relevant to your work, you are welcome to use it to complete the project.
+The following provides more detail:
 
-### Access
-Upload the data to an S3 bucket through the AWS Gateway so that SageMaker has access to the data. 
+1. Training jobs:
+    1. Screenshot of hyperparameter training job:
+    2. Metrics during training 
+    
+    1. Screenshot of best estimator training job with profiler and debugger:
+    2. Metrics during training:
 
-## Hyperparameter Tuning
-What kind of model did you choose for this experiment and why? Give an overview of the types of parameters and their ranges used for the hyperparameter search
 
-Remember that your README should:
-- Include a screenshot of completed training jobs
-- Logs metrics during the training process
-- Tune at least two hyperparameters
-- Retrieve the best best hyperparameters from all your training jobs
+2. Debugging and Profiling
+Debugger helps us understand what is happening when the model is training, this helps us see if ther are issues specifically with vanishing gradients, overfitting and the loss not decreasing. Profiler helps us check how well out model is training and inspect CPU/GPU utilisation , CPU/GPU memory utilisation aswell as instance metrics. This has been perfomed by adding debugger and profiler hooks into the training script. 
 
-## Debugging and Profiling
-**TODO**: Give an overview of how you performed model debugging and profiling in Sagemaker
-
-### Results
+Results
 **TODO**: What are the results/insights did you get by profiling/debugging your model?
+From debugger we can view the cross entropy loss:
+
+The profiler report was saved in S3, please view [this file] to view the report.
 
 **TODO** Remember to provide the profiler html/pdf file in your submission.
 
 
 ## Model Deployment
 **TODO**: Give an overview of the deployed model and instructions on how to query the endpoint with a sample input.
+The model was deployed to an endpoint. Yoy may send dogimages to the endpoint and expect a prediction. 
+
 
 **TODO** Remember to provide a screenshot of the deployed active endpoint in Sagemaker.
 
-## Standout Suggestions
-**TODO (Optional):** This is where you can provide information about any standout suggestions that you have attempted.
+
--- hpo.py
+++ hpo.py
@@ -7,73 +7,268 @@ import torch.optim as optim
 import torchvision
 import torchvision.models as models
 import torchvision.transforms as transforms
-
 import argparse
+import json
+import logging
+import os
+import sys
+import time
 
-def test(model, test_loader):
+# import smdebug.pytorch as smd
+# from smdebug import modes
+# from smdebug.profiler.utils import str2bool
+# from smdebug.pytorch import get_hook
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+def test(model, test_loader,criterion, device):
     '''
     TODO: Complete this function that can take a model and a 
           testing data loader and will get the test accuray/loss of the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    # ===================================================#
+    # 3. Set the SMDebug hook for the validation phase. #
+    # ===================================================#
+    # hook.set_mode(smd.modes.EVAL)
+    print("Testing Model on Whole Testing Dataset")
+    model.eval()
+    running_loss=0
+    running_corrects=0
+    
+    for inputs, labels in test_loader:
+        inputs=inputs.to(device)
+        labels=labels.to(device)
+        outputs=model(inputs)
+        loss=criterion(outputs, labels)
+        _, preds = torch.max(outputs, 1)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data).item()
 
-def train(model, train_loader, criterion, optimizer):
+    total_loss = running_loss / len(test_loader.dataset)
+    total_acc = running_corrects/ len(test_loader.dataset)
+    print(f"Testing Accuracy: {100*total_acc}, average test loss: {total_loss}")
+
+def train(model, train_loader,
+         validation_loader, criterion, 
+         optimizer, args, 
+         device):
+
     '''
     TODO: Complete this function that can take a model and
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    epochs=args.epochs
+    best_loss=1e6
+    image_dataset={'train':train_loader, 'valid':validation_loader}
+    loss_counter=0
+    # =================================================#
+    # 2. Set the SMDebug hook for the training phase. #
+    # =================================================#
+    # hook.set_mode(smd.modes.TRAIN)
+    # hook.register_loss(criterion)  
+
+    for epoch in range(epochs):
+        for phase in ['train', 'valid']:
+            print(f"Epoch {epoch}, Phase {phase}")
+            if phase=='train':
+                # hook.set_mode(modes.TRAIN)
+                model.train()
+            else:
+                # hook.set_mode(modes.EVAL)
+                model.eval()
+            running_loss = 0.0
+            running_corrects = 0
+            running_samples=0
+
+            for step, (inputs, labels) in enumerate(image_dataset[phase]):
+                inputs=inputs.to(device)
+                labels=labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase=='train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data).item()
+                running_samples+=len(inputs)
+                if running_samples % 2000  == 0:
+                    accuracy = running_corrects/running_samples
+                    logger.info("Images [{}/{} ({:.0f}%)] Loss: {:.2f} Accuracy: {}/{} ({:.2f}%) Time: {}".format(
+                            running_samples,
+                            len(image_dataset[phase].dataset),
+                            100.0 * (running_samples / len(image_dataset[phase].dataset)),
+                            loss.item(),
+                            running_corrects,
+                            running_samples,
+                            100.0*accuracy,
+                            time.asctime() # for measuring time for testing, remove for students and in the formatting
+                        )
+                    )
+                
+                #NOTE: Comment lines below to train and test on whole dataset
+                # if running_samples>(0.2*len(image_dataset[phase].dataset)):
+                #     break
+
+            epoch_loss = running_loss / running_samples
+            epoch_acc = running_corrects / running_samples
+            
+            if phase=='valid':
+                if epoch_loss<best_loss:
+                    best_loss=epoch_loss
+                else:
+                    loss_counter+=1
+
+        if loss_counter==1:
+            break
+    return model
+
     
 def net():
     '''
     TODO: Complete this function that initializes your model
           Remember to use a pretrained model
     '''
-    pass
+    model = models.resnet18(pretrained=True)
+    #freeze the convolutional layer 
+    for param in model.parameters():
+        param.requires_grad = False   
+    # then add fully connected layer
+    num_features=model.fc.in_features ##check how many features present in output of the model
+    model.fc = nn.Sequential(
+                nn.Linear(num_features, 133)) 
+    return model
 
-def create_data_loaders(data, batch_size):
+
+def create_data_loaders(args):
     '''
     This is an optional function that you may or may not need to implement
     depending on whether you need to use data loaders or not
     '''
-    pass
 
+    traindir = os.path.join(args.data_dir , 'train')
+    testdir = os.path.join(args.data_dir, 'test')
+    valdir = os.path.join(args.data_dir , 'valid')
+    
+    training_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.Resize((224,244)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    testing_transform = transforms.Compose([
+        transforms.Resize((224, 244)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    trainset = torchvision.datasets.ImageFolder(
+        root=traindir, 
+        transform=training_transform)
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset,shuffle=True, batch_size=args.batch_size)
+
+    validationset = torchvision.datasets.ImageFolder(
+        root=valdir, 
+        transform=testing_transform)
+
+    validationloader = torch.utils.data.DataLoader(
+        validationset,shuffle=False, batch_size=args.batch_size)
+
+    testset = torchvision.datasets.ImageFolder(
+        root=testdir, 
+        transform=testing_transform)
+
+    testloader = torch.utils.data.DataLoader(
+        testset,shuffle=False, batch_size=args.batch_size)
+    
+
+    return trainloader, validationloader, testloader
+ 
 def main(args):
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Running on Device {device}")
     '''
     TODO: Initialize a model by calling the net function
     '''
     model=net()
-    
+    model.to(device)
+
+    # ======================================================#
+    # 4. Register the SMDebug hook to save output tensors. #
+    # ======================================================#
+    # hook = smd.Hook.create_from_json_file()
+    # hook.register_hook(model)
+
     '''
     TODO: Create your loss and optimizer
     '''
-    loss_criterion = None
-    optimizer = None
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=args.lr)
     
     '''
     TODO: Call the train function to start training your model
     Remember that you will need to set up a way to get training data from S3
     '''
-    model=train(model, train_loader, loss_criterion, optimizer)
-    
+    logger.info('create data loaders')
+    trainloader ,validationloader, testloader = create_data_loaders(args)
+    logger.info('training model')
+    model = train(model, trainloader,validationloader, criterion, optimizer, args, device)
+
     '''
     TODO: Test the model to see its accuracy
     '''
-    test(model, test_loader, criterion)
-    
+    logger.info('final model accuracy')
+    test(model, testloader, criterion, device)
+
     '''
     TODO: Save the trained model
     '''
-    torch.save(model, path)
+    logger.info('saving model')  
+    with open(os.path.join(args.model_dir, 'dogmodel.pth'), 'wb') as f:
+        torch.save(model.state_dict(), f)
 
 if __name__=='__main__':
     parser=argparse.ArgumentParser()
     '''
-    TODO: Specify all the hyperparameters you need to use to train your model.
+    TODO: Specify any training args that you might need
     '''
-    
+
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=14,
+        metavar="N",
+        help="number of epochs to train (default: 14)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)"
+    )
+
+
+    parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument('--output_dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_TRAINING"])
+
     args=parser.parse_args()
-    
+
     main(args)
